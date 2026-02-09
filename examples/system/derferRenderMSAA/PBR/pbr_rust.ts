5f92fb915f7214bf70ec1121674b3d35b5c10a2d
bythesword <bythesword@163.com>
2026-02-09 19:29:26 +0800

    20260209 修正一个延迟渲染示例

--- examples/system/derferRenderMSAA/PBR/pbr_rust.ts
+++ examples/system/derferRenderMSAA/PBR/pbr_rust.ts
@@ -119,30 +119,31 @@ let lightMaterial = new ColorMaterial(
 let lightRadius = 1.65;
 let lightRadiusFlag = true;
 let lightZ = 0;
-let light1Entity1 = new Mesh(
+let meshPointBall = new Mesh(
   {
     attributes: {
       geometry: ballGeometry,
     },
     material: lightMaterial,
-    position: [1, 1, 1],
+  });
+
+
+let nodeLight1 = await scene.add(
+  {
+    entity: meshPointBall,
     update: (scope: any) => {
       const now = Date.now() / 500;
       let pos = [Math.sin(now) * lightRadius, lightZ, Math.cos(now) * lightRadius];
       scope.Position = pos;
     }
-  });
-await scene.add(light1Entity1);
-
-
-
-
+  }
+);
 
 let onelight = new PointLight(
   {
-    position: [0,0,0],
+    position: [0, 0, 0],
     // position: [1, 1, 1],
     intensity: 2.0,
   }
 );
-await light1Entity1.addChild(onelight);
+await nodeLight1.addChild(onelight);
